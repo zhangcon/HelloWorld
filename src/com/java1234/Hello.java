7674de8c1d0b73ef014d0e49613562fb5cba122a
zhangcon <942925859@qq.com>
2019-07-31 00:02:34 +0800

    项目上传到GitHub上

---
+++ src/com/java1234/Hello.java
@@ -0,0 +1,10 @@
+package com.java1234;/*
+ *@author 张聪
+ *@date 2019/7/30 - 21:56
+ */
+
+public class hello {
+    public static void main(string[] args) {
+
+    }
+}
---
+++ src/com/java1234/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.java1234;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWorld {
+    /**
+     * 这是注释哦
+     * @param args
+     */
+    public static void main(String[] args ){
+        //你好哦，这个也是注释哦
+        List list = new ArrayList<>();
+    }
+
+
+
+}
